854db55004a699baa34ab408c47053309772de72
sultan alyami <ssl55@hotmail.com>
2022-07-09 05:44:04 +0300

    Add files via upload

---
+++ OpenSSL.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Aes128Decrypt
+{
+    public class OpenSSL
+    {
+        public static string OpenSSLDecrypt(string encrypted, byte[] key, byte[] iv)
+        {
+            // base 64 decode
+            byte[] encryptedBytesWithSalt = Convert.FromBase64String(encrypted);
+            // extract salt (first 8 bytes of encrypted)
+            byte[] salt = new byte[8];
+
+
+
+            byte[] encryptedBytes = new byte[encryptedBytesWithSalt.Length - salt.Length - 8];
+            Buffer.BlockCopy(encryptedBytesWithSalt, 8, salt, 0, salt.Length);
+            Buffer.BlockCopy(encryptedBytesWithSalt, salt.Length + 8, encryptedBytes, 0, encryptedBytes.Length);
+
+            return DecryptStringFromBytesAes(encryptedBytesWithSalt, key, iv);
+        }
+        static string DecryptStringFromBytesAes(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("key");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException("iv");
+
+            // Declare the RijndaelManaged object
+            // used to decrypt the data.
+            RijndaelManaged aesAlg = null;
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext;
+
+            try
+            {
+                // Create a RijndaelManaged object
+                // with the specified key and IV.
+                aesAlg = new RijndaelManaged { Mode = CipherMode.CBC, KeySize = 256, BlockSize = 128, Key = key, IV = iv };
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                            srDecrypt.Close();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            return plaintext;
+        }
+
+
+    }
+}
---
+++ PayloadDecryption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aes128Decrypt
+{
+    public class PayloadDecryption
+    {
+        public static string OpenSSLDecrypt(string payload,string ClientSecret)
+        {
+            string padding= payload.Replace("-", "+").Replace("_", "/");
+            padding = padding.PadRight(padding.Length + (4 - (padding.Length % 4)), '=');
+            var decoded = padding.DecodeBase64;
+            var ivScout = decoded.ToString().Substring(0, 16);
+            ClientSecret = ClientSecret.Substring(0, 16);
+            byte[] iv = Encoding.ASCII.GetBytes(ivScout);
+            byte[] key = Encoding.ASCII.GetBytes(ClientSecret);
+
+            var json = OpenSSL.OpenSSLDecrypt(padding, key, iv);
+
+            var revised = json.Substring(json.IndexOf('{'));
+            return revised; 
+        }
+
+    }
+}
